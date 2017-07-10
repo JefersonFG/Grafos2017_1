a2465453919c1930c45943f1b60996d79bde3a02
JefersonFG <jefersonfguimaraes@gmail.com>
2017-07-10 00:32:00 -0300

    Processado input conforme especificação do trabalho

--- main.c
+++ main.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 #include "persistence.h"
 #include "graph_processing.h"
 
-int main() {
-    printf("Debug: main\n");
-    getGraphSize("Teste");
-    readInputFile("Teste", 0);
-    numOfConnectedComponentsBfs(0, 0, 0);
-    numOfConnectedComponentsDfs(0, 0, 0);
-    distancesBfs(0, 0, 0);
-    distancesDfs(0, 0, 0);
+/// Função principal do programa, processa os argumentos e exibe o resultado na tela.
+/// \param argc Número de argumentos passados, que deve ser 4 (main, algoritmo, função e arquivo)
+/// \param argv Argumentos passados para o programa.
+/// \return Em geral, 0 para indicar que o programa finalizou corretamente.
+int main(int argc, char *argv[]) {
+    int vertices;
+    int edges;
+    int graphSize[2];
+    int* graphEdges;
+
+    if (argc != 4) {
+        printf("Wrong input, supported format: ./main -algorithm -function filename");
+        return 0;
+    }
+
+    getGraphSize(argv[3]);
+    readInputFile(argv[3], 0);
+
+    if (!strcmp(argv[1], "-bfs")) {
+        if (!strcmp(argv[2], "-c")) {
+            numOfConnectedComponentsBfs(0, 0, 0);
+        } else {
+            distancesBfs(0, 0, 0);
+        }
+    } else {
+        if (!strcmp(argv[2], "-c")) {
+            numOfConnectedComponentsDfs(0, 0, 0);
+        } else {
+            distancesDfs(0, 0, 0);
+        }
+    }
+
     return 0;
 }
--- persistence.c
+++ persistence.c
@@ -8,8 +8,11 @@
 /// \param fileName Nome do arquivo de entrada contendo o grafo.
 /// \return Array com o número de vértices do grafo na primeira posição e número de arestas na segunda posição.
 int* getGraphSize(char* fileName) {
-    printf("Debug: getGraphSize\n");
-    return 0;
+    int* graphSize;
+
+    printf("Debug: getGraphSize - %s\n", fileName);
+
+    return graphSize;
 }
 
 /// Lê o arquivo de entrada retornando uma matriz que contém as arestas do grafo.
@@ -17,6 +20,9 @@ int* getGraphSize(char* fileName) {
 /// \param numEdges Número de arestas do grafo.
 /// \return Matriz com as arestas do grafo lidas do arquivo de entrada.
 int** readInputFile(char* fileName, int numEdges) {
-    printf("Debug: readInputFile\n");
-    return 0;
+    int** graphEdges;
+
+    printf("Debug: readInputFile - %s\n", fileName);
+
+    return graphEdges;
 }
