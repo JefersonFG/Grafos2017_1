1598094fb56fde54d941041631fb4ffead0f996c
João Teló <joaotelo.nh@hotmail.com>
2017-07-11 17:11:53 -0300

    BFS distances implemented

--- graph_processing.c
+++ graph_processing.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "graph_processing.h"
+#include "queue.h"
 
 int numOfConnectedComponentsBfs(struct Graph *graph, int numVertices, int numEdges) {
     printf("Debug: numOfConnectedComponentsBfs\n");
@@ -12,8 +14,30 @@ int numOfConnectedComponentsDfs(struct Graph *graph, int numVertices, int numEdg
 }
 
 int* distancesBfs(struct Graph *graph, int numVertices, int numEdges) {
+    int * distances = (int *) malloc(sizeof(int) * numVertices);
+    struct Queue * queue = initializeQueue();
+    struct node * current;
+    int i, v, w;
+
+    for (i = 0; i < numVertices; i++){
+        distances[i] = -1;
+    }
+    distances[0] = 0; // Todas distâncias serão calculadas a partir do nodo 0
+    enqueue(queue, 0);
+
+    while (!isEmptyQueue(queue)){
+        v = dequeue(queue);
+        for (current = graph->adjLists[v]; current != NULL; current = current->next){
+            w = current->vertex;
+            if (distances[w] == -1) {
+                distances[w] = distances[v] + 1;
+                enqueue(queue, w);
+            }
+        }
+    }
+
     printf("Debug: distancesBfs\n");
-    return NULL;
+    return distances;
 }
 
 int* distancesDfs(struct Graph *graph, int numVertices, int numEdges) {
