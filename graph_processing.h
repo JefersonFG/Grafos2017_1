590080a4ed9ad06a8fae1e210c2ba12b7ccbffac
Caio Roberto <caiorrs@gmail.com>
2017-07-16 15:11:47 -0300

    Update graph_processing.h

--- graph_processing.h
+++ graph_processing.h
@@ -7,6 +7,13 @@
 #ifndef GRAFOS2017_1_GRAPH_PROCESSING_H
 #define GRAFOS2017_1_GRAPH_PROCESSING_H
 
+/// Função utilizada para marcar os vértices passados pelo DFS
+/// \ param numVertices Número de vértices do grafo
+/// \ param node Nodo a partir do qual será calculado o DFS
+/// \ param isNodeExplored[] Vetor que marca os nodos já visitados
+/// \ param numVertices Serve para controlar se todos os nodos foram explorados ou não
+void DFS(int node, isNodeExplored[], int numVertices);
+
 /// Retorna o número de componentes conexos utilizando o algoritmo bfs.
 /// \param graph Grafo a ser processado.
 /// \param numVertices Número de vértices do grafo.
